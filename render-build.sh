c97daf3221ed66694f3751a22e243e685ba66118
Mustafa <153886730+MustafaRaheel26@users.noreply.github.com>
2026-05-18 02:13:58 +0500

    Simplify WeasyPrint dependencies installation

--- render-build.sh
+++ render-build.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install system dependencies for WeasyPrint
-apt-get update && apt-get install -y \
-    poppler-utils \
-    libpango-1.0-0 \
-    libpangoft2-1.0-0 \
-    libffi-dev \
-    libcairo2 \
-    libgdk-pixbuf2.0-0 \
-    libxml2-dev \
-    libxslt1-dev
+apt-get update && apt-get install -y poppler-utils
 
 pip install --upgrade pip
 pip install -r requirements.txt
