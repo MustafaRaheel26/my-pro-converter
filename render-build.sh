f6dcb1ab3ec3065a13b3d85dc1c531b5f5bf477e
Mustafa <153886730+MustafaRaheel26@users.noreply.github.com>
2026-05-18 01:47:05 +0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install poppler for PDF to PPT
-apt-get update && apt-get install -y poppler-utils
+# Install system dependencies for WeasyPrint (and poppler for pdf2image)
+apt-get update && apt-get install -y \
+    poppler-utils \
+    libpango-1.0-0 \
+    libpangoft2-1.0-0 \
+    libffi-dev \
+    libcairo2 \
+    libgdk-pixbuf2.0-0 \
+    libxml2-dev \
+    libxslt1-dev
 
 # Upgrade pip
 pip install --upgrade pip
 
-# Install requirements
+# Install Python requirements
 pip install -r requirements.txt
